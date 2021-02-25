e390c8d354578d55a7b3f8b58e044f84c6a95119
imnothacqer <51673762+imnothacqer@users.noreply.github.com>
2021-02-25 12:29:36 +0300

    Import Models and Complete Player Movement Script
    Inported Models :
        +Player Model
        +Curved Cube Model

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool canMove;
+    public bool canSlide;
+
+    public float movementSpeed;
+    public float slideSensitivity;
+
+    public Vector3 moveDirection;
+    public Vector3 slideDirection;
+
+    private float inputHorizontal;
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            transform.Translate(moveDirection * movementSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void Update()
+    {
+        if (canSlide)
+        {
+            inputHorizontal = SimpleInput.GetAxis("Horizontal");
+            Vector3 mov = slideDirection * (inputHorizontal * slideSensitivity);
+            transform.Translate(mov * Time.deltaTime);
+        }
+    }
+}
